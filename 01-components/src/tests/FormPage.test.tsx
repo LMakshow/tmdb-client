dbb1aa58a74037f2b5a634200d162b8fc5c01c81
Maksym Lytvyn <makshow.at@gmail.com>
2022-10-02 18:40:34 +0300

    feat: add tests to render popular movies and modal

--- 01-components/src/tests/FormPage.test.tsx
+++ 01-components/src/tests/FormPage.test.tsx
@@ -15,10 +15,7 @@ describe('Form Page (with user generated cards)', () => {
   test('creates new card after submitting', () => {
     render(<RouterProvider router={router} />);
 
-    expect(screen.getAllByRole('button')[0]).toBeInTheDocument();
-    expect(screen.getAllByRole('button')[0]).toBeDisabled();
     userEvent.type(screen.getByRole('textbox'), 'New Camera Card');
-    expect(screen.getAllByRole('button')[0]).not.toBeDisabled();
     userEvent.click(screen.getAllByRole('button')[0]);
     expect(screen.getByText(/New Camera Card/)).toBeInTheDocument();
   });
--- 01-components/src/tests/Main.test.tsx
+++ 01-components/src/tests/Main.test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Main from 'components/Main';
+import userEvent from '@testing-library/user-event';
 
 const router = createBrowserRouter([
   {
@@ -10,11 +11,30 @@ const router = createBrowserRouter([
   },
 ]);
 
+// describe('Main', () => {
+//   test('renders Main component', () => {
+//     render(<RouterProvider router={router} />);
+
+//     expect(screen.getByText('Canon EOS 4000D 18-55 DC III')).toBeInTheDocument();
+//     expect(screen.getByText('Canon EOS 5D Mark IV 24-105L IS II')).toBeInTheDocument();
+//   });
+// });
+
 describe('Main', () => {
-  test('renders Main component', () => {
+  test('renders popular movies cards and render votes number', async () => {
     render(<RouterProvider router={router} />);
 
-    expect(screen.getByText('Canon EOS 4000D 18-55 DC III')).toBeInTheDocument();
-    expect(screen.getByText('Canon EOS 5D Mark IV 24-105L IS II')).toBeInTheDocument();
+    expect(screen.getByText(/Browse the most popular movies/)).toBeInTheDocument();
+    await waitFor(() => screen.getByText(/Orphan: First Kill/));
+    expect(screen.getByText(/797/)).toBeInTheDocument();
+  });
+
+  test('shows modal window after a click on a card and renders release date', async () => {
+    render(<RouterProvider router={router} />);
+
+    await waitFor(() => screen.getByText(/Orphan: First Kill/));
+    userEvent.click(screen.getByText(/Orphan: First Kill/));
+    await waitFor(() => screen.getByText(/There's always been something wrong with Esther/));
+    expect(screen.getByText(/2022-07-27/)).toBeInTheDocument();
   });
 });
