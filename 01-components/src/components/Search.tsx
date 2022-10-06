9ba6bcb0094cfcd26da31b1a7d38aa7ef743c13a
Maksym Lytvyn <makshow.at@gmail.com>
2022-10-06 15:46:28 +0300

    refactor: change Search to functional component

--- 01-components/src/components/Search.tsx
+++ 01-components/src/components/Search.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 interface SearchProps {
   onQueryChange: (query: string) => void;
@@ -7,35 +7,33 @@ interface SearchProps {
   searchQuery: string;
 }
 
-export default class Search extends React.Component<SearchProps, unknown> {
-  handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.props.onQueryChange(e.target.value);
+export default function Search(props: SearchProps) {
+  const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.onQueryChange(e.target.value);
   };
 
-  handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    this.props.onSearchSubmit();
+  const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    props.onSearchSubmit();
     e.preventDefault();
   };
 
-  render() {
-    return (
-      <div className="search-container">
-        {/* <Link to="/form">
-          <button className="button_big">Browse used cameras</button>
-        </Link> */}
-        <form className="search-field" onSubmit={this.handleSearchSubmit}>
-          <input
-            className="search-box"
-            type="search"
-            placeholder="Search"
-            autoComplete="off"
-            autoFocus
-            value={this.props.searchQuery}
-            onChange={this.handleQueryChange}
-          />
-          <button type="submit" className="search-submit"></button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className="search-container">
+      <Link to="/form">
+        <button className="button_big">Add movie request</button>
+      </Link>
+      <form className="search-field" onSubmit={handleSearchSubmit}>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search"
+          autoComplete="off"
+          autoFocus
+          value={props.searchQuery}
+          onChange={handleQueryChange}
+        />
+        <button type="submit" className="search-submit"></button>
+      </form>
+    </div>
+  );
 }
