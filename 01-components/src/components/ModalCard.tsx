12633f76c854c95b8a000d31dc478295729343f9
Maksym Lytvyn <makshow.at@gmail.com>
2022-10-06 16:11:32 +0300

    refactor: change cards to functions with useRef

--- 01-components/src/components/ModalCard.tsx
+++ 01-components/src/components/ModalCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { MovieDetails } from 'utils/TMDBinterfaces';
 import closeBtn from '../assets/svg/close.svg';
 import { NetworkError, Preloader } from './Network';
@@ -10,21 +10,16 @@ interface ModalCardProps {
   toggleModal: () => void;
 }
 
-export default class ModalCard extends React.Component<ModalCardProps, unknown> {
-  modalRef: React.RefObject<HTMLDivElement>;
+export default function ModalCard(props: ModalCardProps) {
+  const modalRef = useRef(null);
 
-  constructor(props: ModalCardProps) {
-    super(props);
-    this.modalRef = React.createRef();
-  }
-
-  handleClickOutside = (e: React.MouseEvent<HTMLDivElement>) => {
-    if (this.modalRef.current === e.target) this.props.toggleModal();
+  const handleClickOutside = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (modalRef.current === e.target) props.toggleModal();
   };
 
-  genresCards = () => {
-    if (!this.props.data?.genres) return null;
-    const cards = this.props.data?.genres.map((item) => (
+  const genresCards = () => {
+    if (!props.data?.genres) return null;
+    const cards = props.data?.genres.map((item) => (
       <div key={item.id} className="movie-card__genre">
         {item.name}
       </div>
@@ -32,9 +27,9 @@ export default class ModalCard extends React.Component<ModalCardProps, unknown> 
     return cards;
   };
 
-  videosCards = () => {
-    if (!this.props.data?.videos.results) return null;
-    let cards = this.props.data?.videos.results.map((item) => (
+  const videosCards = () => {
+    if (!props.data?.videos.results) return null;
+    let cards = props.data?.videos.results.map((item) => (
       <a
         key={item.id}
         className="movie-card__genre"
@@ -51,76 +46,78 @@ export default class ModalCard extends React.Component<ModalCardProps, unknown> 
     return cards;
   };
 
-  loadingCard = () => {
+  const loadingCard = () => {
     return (
-      <div className="modal-overlay" ref={this.modalRef} onClick={this.handleClickOutside}>
+      <div className="modal-overlay" ref={modalRef} onClick={handleClickOutside}>
         <Preloader />
       </div>
     );
   };
 
-  errorCard = (message: string) => {
+  const errorCard = (message: string) => {
     return (
-      <div className="modal-overlay" ref={this.modalRef} onClick={this.handleClickOutside}>
+      <div className="modal-overlay" ref={modalRef} onClick={handleClickOutside}>
         <NetworkError message={message} />
       </div>
     );
   };
 
-  render() {
-    if (!this.props.show) return null;
-    if (this.props.error) return this.errorCard(this.props.error as string);
-    if (!this.props.data) return this.loadingCard();
-    return (
-      <div className="modal-overlay" ref={this.modalRef} onClick={this.handleClickOutside}>
-        <div
-          className="modal-card"
-          style={{
-            background: `linear-gradient(rgba(255,255,255,0.8),rgba(255,255,255,0.8)), 
-            url("https://image.tmdb.org/t/p/w780${this.props.data?.backdrop_path}") center/cover`,
-          }}
-        >
-          <div className="modal-card__button" onClick={this.props.toggleModal}>
-            <img src={closeBtn} alt="&times;" />
-          </div>
-          <img
-            className="modal-card__poster"
-            src={`https://image.tmdb.org/t/p/w500${this.props.data?.poster_path}`}
-          />
-          <div className="modal-card__text">
-            <h3 className="modal-card__movie-name">{this.props.data?.title}</h3>
-            <div className="modal-card__movie-tagline">{this.props.data?.tagline}</div>
-            <div className="modal-card__movie-overview">{this.props.data?.overview}</div>
-            <div className="movie-card__ratings">
-              <div className="movie-card__block">
-                <span className="text-aux">🠕</span>
-                <p className="movie-card__popularity text-big">
-                  {Math.round(this.props.data?.popularity as number)}
-                </p>
-                <span className="text-aux">Popularity</span>
-              </div>
-              <div className="movie-card__block">
-                <h3 className="movie-card__votes text-big">{this.props.data?.vote_count}</h3>
-                <span className="text-aux">votes</span>
-              </div>
-              <div className="movie-card__block">
-                <span className="text-aux color-accent">❤</span>
-                <p className="text-big">{this.props.data?.vote_average}</p>
-              </div>
+  if (!props.show) return null;
+  if (props.error) return errorCard(props.error as string);
+  if (!props.data) return loadingCard();
+  return (
+    <div className="modal-overlay" ref={modalRef} onClick={handleClickOutside}>
+      <div
+        className="modal-card"
+        style={{
+          background: `linear-gradient(rgba(255,255,255,0.8),rgba(255,255,255,0.8)), 
+            url("https://image.tmdb.org/t/p/w780${props.data?.backdrop_path}") center/cover`,
+        }}
+      >
+        <div className="modal-card__button" onClick={props.toggleModal}>
+          <img src={closeBtn} alt="&times;" />
+        </div>
+        <img
+          className="modal-card__poster"
+          src={
+            props.data.poster_path === null
+              ? `${process.env.PUBLIC_URL}/cameras/no-image.jpg`
+              : `https://image.tmdb.org/t/p/w500${props.data.poster_path}`
+          }
+        />
+        <div className="modal-card__text">
+          <h3 className="modal-card__movie-name">{props.data?.title}</h3>
+          <div className="modal-card__movie-tagline">{props.data?.tagline}</div>
+          <div className="modal-card__movie-overview">{props.data?.overview}</div>
+          <div className="movie-card__ratings">
+            <div className="movie-card__block">
+              <span className="text-aux">🠕</span>
+              <p className="movie-card__popularity text-big">
+                {Math.round(props.data?.popularity as number)}
+              </p>
+              <span className="text-aux">Popularity</span>
             </div>
             <div className="movie-card__block">
-              <span className="text-med">Release date:</span>
-              <span className="text-big">{this.props.data?.release_date}</span>
+              <h3 className="movie-card__votes text-big">{props.data?.vote_count}</h3>
+              <span className="text-aux">votes</span>
             </div>
             <div className="movie-card__block">
-              <span className="text-med">Genres:</span>
-              {this.genresCards()}
+              <span className="text-aux color-accent">❤</span>
+              <p className="text-big">{props.data?.vote_average}</p>
             </div>
-            <p className="text-med">Watch Videos:</p>
-            <div className="movie-card__block scroll-y">{this.videosCards()}</div>
           </div>
+          <div className="movie-card__block">
+            <span className="text-med">Release date:</span>
+            <span className="text-big">{props.data?.release_date}</span>
+          </div>
+          <div className="movie-card__block">
+            <span className="text-med">Genres:</span>
+            {genresCards()}
+          </div>
+          <p className="text-med">Watch Videos:</p>
+          <div className="movie-card__block scroll-y">{videosCards()}</div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
--- 01-components/src/components/MovieCard.tsx
+++ 01-components/src/components/MovieCard.tsx
@@ -5,43 +5,36 @@ interface MovieCardProps extends MovieData {
   onClick: (id: number) => Promise<void>;
 }
 
-export default class MovieCard extends React.Component<MovieCardProps, { cart: boolean }> {
-  constructor(props: MovieCardProps) {
-    super(props);
-    this.state = { cart: false };
-  }
-
-  handleClick = () => {
-    this.props.onClick(this.props.id);
+export default function MovieCard(props: MovieCardProps) {
+  const handleClick = () => {
+    props.onClick(props.id);
   };
 
-  render() {
-    return (
-      <div className="movie-card-template" onClick={this.handleClick}>
-        <div className="shop-card movie-card">
-          <img
-            className="movie-card__image"
-            src={
-              this.props.poster_path === null
-                ? `${process.env.PUBLIC_URL}/cameras/no-image.jpg`
-                : `https://image.tmdb.org/t/p/w500${this.props.poster_path}`
-            }
-            alt="Photo"
-          />
-          <h2 className="movie-card__name">{this.props.title}</h2>
+  return (
+    <div className="movie-card-template" onClick={handleClick}>
+      <div className="shop-card movie-card">
+        <img
+          className="movie-card__image"
+          src={
+            props.poster_path === null
+              ? `${process.env.PUBLIC_URL}/cameras/no-image.jpg`
+              : `https://image.tmdb.org/t/p/w500${props.poster_path}`
+          }
+          alt="Photo"
+        />
+        <h2 className="movie-card__name">{props.title}</h2>
 
-          <div className="movie-card__ratings">
-            <div className="movie-card__block">
-              <h3 className="movie-card__votes text-big">{this.props.vote_count}</h3>
-              <span className="text-aux">votes</span>
-            </div>
-            <div className="movie-card__block">
-              <span className="text-aux color-accent">❤</span>
-              <p className="text-big">{this.props.vote_average}</p>
-            </div>
+        <div className="movie-card__ratings">
+          <div className="movie-card__block">
+            <h3 className="movie-card__votes text-big">{props.vote_count}</h3>
+            <span className="text-aux">votes</span>
+          </div>
+          <div className="movie-card__block">
+            <span className="text-aux color-accent">❤</span>
+            <p className="text-big">{props.vote_average}</p>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
