b419e47ba3189f672b40f87383d7cab12b56c8fd
Maksym Lytvyn <makshow.at@gmail.com>
2022-10-06 15:54:36 +0300

    refactor: continue change to functional components

--- 01-components/src/components/Header.tsx
+++ 01-components/src/components/Header.tsx
@@ -2,21 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logoMain from '../assets/svg/TMDB.svg';
 
-export default class Header extends React.Component<{ pageName: string }, unknown> {
-  render() {
-    return (
-      <div className="header header__container">
-        <h1 className="navigation">
-          <Link className="header__link main" to="/">
-            <img className="header__icon_main" src={logoMain} alt="Главная" />
-            <span className="header__link_span">TMDB</span>
-          </Link>
-        </h1>
-        <div className="navigation">{this.props.pageName}</div>
-        <Link className="navigation header__link main" to="/about">
-          <span className="header__link_span">About us</span>
+export default function Header(props: { pageName: string }) {
+  return (
+    <div className="header header__container">
+      <h1 className="navigation">
+        <Link className="header__link main" to="/">
+          <img className="header__icon_main" src={logoMain} alt="Главная" />
+          <span className="header__link_span">TMDB</span>
         </Link>
-      </div>
-    );
-  }
+      </h1>
+      <div className="navigation">{props.pageName}</div>
+      <Link className="navigation header__link main" to="/about">
+        <span className="header__link_span">About us</span>
+      </Link>
+    </div>
+  );
 }
--- 01-components/src/components/Heading.tsx
+++ 01-components/src/components/Heading.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export default class Heading extends React.Component<{ text: string }, unknown> {
-  render() {
-    return (
-      <div className="heading header__container">
-        <h2 className="navigation">{this.props.text}</h2>
-      </div>
-    );
-  }
+export default function Heading(props: { text: string }) {
+  return (
+    <div className="heading header__container">
+      <h2 className="navigation">{props.text}</h2>
+    </div>
+  );
 }
